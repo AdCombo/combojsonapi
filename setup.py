d0aadfa8ab6dbca02b4cb1ed65ea464c3e788955
Aleksey Nekrasov <aleks.nekrasov@it.adcombo.com>
2019-07-05 11:46:03 +0300

    Updated marshmallow>=3.0.0a1,<=3.0.0rc7

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+__version__ = '0.30.1'
+
+
+setup(
+    name="Flask-REST-JSONAPI",
+    version=__version__,
+    description='Flask extension to create REST web api according to JSONAPI 1.0 specification with Flask, Marshmallow \
+                 and data provider of your choice (SQLAlchemy, MongoDB, ...)',
+    url='https://github.com/miLibris/flask-rest-jsonapi',
+    author='miLibris API Team',
+    author_email='pf@milibris.net',
+    license='MIT',
+    classifiers=[
+        'Framework :: Flask',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: MIT License',
+    ],
+    keywords='web api rest jsonapi flask sqlalchemy marshmallow',
+    packages=find_packages(exclude=['tests']),
+    zip_safe=False,
+    platforms='any',
+    install_requires=['six',
+                      'Flask>=0.11',
+                      'marshmallow>=3.0.0a1,<=3.0.0rc7',
+                      'marshmallow_jsonapi',
+                      'apispec>=1.1.1',
+                      'sqlalchemy'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    extras_require={'tests': 'pytest', 'docs': 'sphinx'}
+)
