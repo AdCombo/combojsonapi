5cd07f5f435e630dee83a75bdf326f56c73bd1e3
Suren Khorenyan <khorenyan@it.adcombo.com>
2019-08-30 20:26:11 +0300

    Create status util

--- flask_rest_jsonapi/utils/__init__.py
+++ flask_rest_jsonapi/utils/__init__.py
@@ -1,9 +1,10 @@
+from .http_status import status
 from .json_encoder import JSONEncoder
 from .schema_name import create_schema_name
 from .decorators import get_decorators_for_resource
 
-
 __all__ = [
+    'status',
     'JSONEncoder',
     'create_schema_name',
     'get_decorators_for_resource',
---
+++ flask_rest_jsonapi/utils/http_status.py
@@ -0,0 +1,9 @@
+from http import HTTPStatus
+
+
+status = {
+    status: {status.value: {'description': status.name.replace('_', ' ').capitalize()}}
+    for status in HTTPStatus
+}
+
+status[HTTPStatus.OK][HTTPStatus.OK.value]['description'] = 'Success'
