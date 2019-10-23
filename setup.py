9556c57ac8668639eedea4860e84b1f3e4060e78
Suren Khorenyan <khorenyan@it.adcombo.com>
2019-10-23 15:34:52 +0300

    Configure setup

--- setup.py
+++ setup.py
@@ -1,17 +1,44 @@
+import pathlib
+
 from setuptools import setup, find_packages
 
-
 __version__ = '0.1.0'
+
+
+try:
+    from pip.req import parse_requirements
+except ImportError:  # pip >= 10.0.0
+    from pip._internal.req import parse_requirements
+
+
+WORK_DIR = pathlib.Path(__file__).parent
+
+
+def get_requirements(filename=None):
+    """
+    https://stackoverflow.com/questions/14399534/reference-requirements-txt-for-the-install-requires-kwarg-in-setuptools-setup-py
+
+    Read requirements from 'requirements txt'
+    :return: requirements
+    :rtype: list
+    """
+    if filename is None:
+        filename = 'requirements.txt'
+
+    file = WORK_DIR / filename
+
+    install_reqs = parse_requirements(str(file), session='hack')
+    return [str(ir.req) for ir in install_reqs]
 
 
 setup(
     name="ComboJSONAPI",
     version=__version__,
-    description='Flask-REST-JSONAPI extension to create web api',
-    url='',
+    description='REST JSONAPI extension to create web api (currently only flask is supported)',  # TODO: separate desription for pypi
+    url='',  # TODO
     author='AdCombo API Team',
-    author_email='',
-    license='MIT',
+    author_email='',  # TODO
+    license='MIT',  # TODO: discuss
     classifiers=[
         'Framework :: Flask',
         'Programming Language :: Python :: 3.6',
@@ -21,11 +48,7 @@ setup(
     packages=find_packages(exclude=['tests']),
     zip_safe=False,
     platforms='any',
-    install_requires=['Flask>=1.0.2',
-                      'flask-rest-jsonapi',
-                      'marshmallow_jsonapi',
-                      'apispec>=2.0.2',
-                      'sqlalchemy'],
+    install_requires=get_requirements(),
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     extras_require={'tests': 'pytest', 'docs': 'sphinx'}
