5888f69630deec4b8e39830c868095d8423ee363
Nekrasov <aleks.nekrasov@it.adcombo.com>
2019-11-06 13:57:17 +0300

    Add SchemaJSONB in __init__.py

--- combojsonapi/postgresql_jsonb/__init__.py
+++ combojsonapi/postgresql_jsonb/__init__.py
@@ -1,6 +1,9 @@
+from combojsonapi.postgresql_jsonb.schema import SchemaJSONB
+
 from combojsonapi.postgresql_jsonb.plugin import PostgreSqlJSONB, is_seq_collection
 
 __all__ = [
     'PostgreSqlJSONB',
     'is_seq_collection',
+    'SchemaJSONB',
 ]
