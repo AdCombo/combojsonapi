d3814c88652eafdd6345820fb2b736998c46c1d7
Sergey Chernyak <sergey.ch@it.adcombo.com>
2020-05-05 19:34:16 +0300

    issue-15 fixed install for a pip >= 20.1

--- setup.py
+++ setup.py
@@ -1,22 +1,27 @@
 import pathlib
+import pip
 
 from setuptools import setup, find_packages
 
 __version__ = "1.0.0"
 
+from packaging import version
 
 try:
     from pip.req import parse_requirements
 except ImportError:  # pip >= 10.0.0
     from pip._internal.req import parse_requirements
 
-
 WORK_DIR = pathlib.Path(__file__).parent
+
+NEW_PIP_REQ_VERSION = '20.1'
+"""Minimal version with a new broken attribute for parse requirements file."""
 
 
 def get_requirements(filename=None):
     """
-    https://stackoverflow.com/questions/14399534/reference-requirements-txt-for-the-install-requires-kwarg-in-setuptools-setup-py
+    https://stackoverflow.com/questions/14399534/reference-requirements-txt-for-the-install-requires-kwarg-in
+    -setuptools-setup-py
 
     Read requirements from 'requirements txt'
     :return: requirements
@@ -26,15 +31,19 @@ def get_requirements(filename=None):
         filename = "requirements.txt"
 
     file = WORK_DIR / filename
-
+    if version.parse(pip.__version__) >= version.parse(NEW_PIP_REQ_VERSION):
+        attr = 'requirement'
+    else:
+        attr = 'req'
     install_reqs = parse_requirements(str(file), session="hack")
-    return [str(ir.req) for ir in install_reqs]
+    return [str(getattr(ir, attr)) for ir in install_reqs]
 
 
 setup(
     name="ComboJSONAPI",
     version=__version__,
-    description="REST JSONAPI extension to create web api (currently only flask is supported)",  # TODO: separate desription for pypi
+    description="REST JSONAPI extension to create web api (currently only flask is supported)",
+    # TODO: separate desription for pypi
     url="https://github.com/AdCombo/combojsonapi",
     author="AdCombo API Team",
     author_email="",  # TODO
@@ -52,6 +61,7 @@ setup(
     platforms="any",
     # this would'n work now
     install_requires=get_requirements(),
+    # todo maybe just write all requirements here? stop usin requirements.txt?
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     extras_require={"tests": "pytest", "docs": "sphinx"},
