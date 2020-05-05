f167e02ac081e431b46f1918d75e02530e1da473
Sergey Chernyak <sergey.ch@it.adcombo.com>
2020-05-05 21:04:25 +0300

    issue-15 move requirements into setup.py

--- setup.py
+++ setup.py
@@ -1,43 +1,6 @@
-import pathlib
-import pip
-
 from setuptools import setup, find_packages
 
-__version__ = "1.0.0"
-
-from packaging import version
-
-try:
-    from pip.req import parse_requirements
-except ImportError:  # pip >= 10.0.0
-    from pip._internal.req import parse_requirements
-
-WORK_DIR = pathlib.Path(__file__).parent
-
-NEW_PIP_REQ_VERSION = '20.1'
-"""Minimal version with a new broken attribute for parse requirements file."""
-
-
-def get_requirements(filename=None):
-    """
-    https://stackoverflow.com/questions/14399534/reference-requirements-txt-for-the-install-requires-kwarg-in
-    -setuptools-setup-py
-
-    Read requirements from 'requirements txt'
-    :return: requirements
-    :rtype: list
-    """
-    if filename is None:
-        filename = "requirements.txt"
-
-    file = WORK_DIR / filename
-    if version.parse(pip.__version__) >= version.parse(NEW_PIP_REQ_VERSION):
-        attr = 'requirement'
-    else:
-        attr = 'req'
-    install_reqs = parse_requirements(str(file), session="hack")
-    return [str(getattr(ir, attr)) for ir in install_reqs]
-
+__version__ = "1.0.2"
 
 setup(
     name="ComboJSONAPI",
@@ -59,9 +22,15 @@ setup(
     packages=find_packages(exclude=["tests"]),
     zip_safe=False,
     platforms="any",
-    # this would'n work now
-    install_requires=get_requirements(),
-    # todo maybe just write all requirements here? stop usin requirements.txt?
+    # this would'n work now Suren, what wil not work now?:)
+    install_requires=[
+        'sqlalchemy',
+        'marshmallow==3.2.1',
+        'marshmallow_jsonapi==0.22.0',
+        'Flask>=1.0.1',
+        'apispec>=2.0.2',
+        'flask-combo-jsonapi @ git+https://github.com/AdCombo/flask-combo-jsonapi.git@1.0.0',
+    ],
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     extras_require={"tests": "pytest", "docs": "sphinx"},
