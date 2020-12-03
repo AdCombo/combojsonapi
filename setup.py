78c11e0687d8886c2ad46d87f9c7bd9ad3932026
Dmitry Mishanov <dmitry.mishanov@it.adcombo.com>
2020-12-03 15:50:43 +0300

    prepare to publish

--- setup.py
+++ setup.py
@@ -1,35 +1,38 @@
+import os
+
 from setuptools import setup, find_packages
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
+
+
+requirements_filepath = os.path.join(os.path.dirname(__name__), "requirements.txt")
+with open(requirements_filepath) as fp:
+    install_requires = fp.read()
+
 
 setup(
     name="ComboJSONAPI",
     version=__version__,
-    description="REST JSONAPI extension to create web api (currently only flask is supported)",
-    # TODO: separate desription for pypi
+    description="4 plugins for Flask-COMBO-JSONAPI package.",
     url="https://github.com/AdCombo/combojsonapi",
-    author="AdCombo API Team",
-    author_email="",  # TODO
-    license="MIT",  # TODO: discuss
+    author="AdCombo Team",
+    author_email="roman@adcombo.com",
+    license="MIT",
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Flask",
+        "Operating System :: OS Independent",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
+        "Topic :: Internet",
     ],
     keywords="web api rest rpc swagger jsonapi flask sqlalchemy marshmallow plugin",
     packages=find_packages(exclude=["tests"]),
     zip_safe=False,
     platforms="any",
-    install_requires=[
-        'sqlalchemy',
-        'marshmallow==3.2.1',
-        'marshmallow_jsonapi==0.22.0',
-        'Flask>=1.0.1',
-        'apispec>=2.0.2',
-        'flask-combo-jsonapi @ git+https://github.com/AdCombo/flask-combo-jsonapi.git@1.0.0',
-    ],
+    install_requires=install_requires,
     setup_requires=["pytest-runner"],
     tests_require=["pytest"],
     extras_require={"tests": "pytest", "docs": "sphinx"},
