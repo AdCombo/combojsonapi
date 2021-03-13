a9a0cf290e1ab5e4195a54cafa85c98f5ac899b1
Suren Khorenyan <surenkhorenyan@gmail.com>
2021-03-13 16:26:18 +0300

    Fixes for apispec processing query params description with schema reference
    https://swagger.io/docs/specification/describing-parameters/

--- combojsonapi/utils/schema_name.py
+++ combojsonapi/utils/schema_name.py
@@ -3,17 +3,25 @@ from apispec.ext.marshmallow import resolver
 from apispec.ext.marshmallow.common import MODIFIERS
 
 
+def schema_not_in_registry(schema_name: str) -> bool:
+    return schema_name not in class_registry._registry
+
+
 def create_schema_name(schema=None, name_schema=None):
     if name_schema:
-        if name_schema not in class_registry._registry:
-            raise ValueError(f"No schema {name_schema}")
+        if schema_not_in_registry(name_schema):
+            raise ValueError(f"No schema {name_schema!r} found in registry!")
         cls_schema = class_registry.get_class(name_schema)
         schema = cls_schema()
     elif schema:
         schema = schema if isinstance(schema, Schema) else schema()
         cls_schema = type(schema)
+    else:
+        raise ValueError("one of params `schema` or `name_schema` required")
+
     if not isinstance(schema, Schema):
         raise TypeError("can only make a schema key based on a Schema instance.")
+
     modifiers = []
     for modifier in MODIFIERS:
         attribute = getattr(schema, modifier)
